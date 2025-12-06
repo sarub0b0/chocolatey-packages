b3caf084095e1fdfd7770d8702335903fd638b02
kosay <ekr59uv25@gmail.com>
2025-12-06 17:20:09 +0900

    kubetui: Update to version 1.11.0

--- kubetui/tools/chocolateyinstall.ps1
+++ kubetui/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url        = 'https://github.com/sarub0b0/kubetui/releases/download/v1.10.2/kubetui-x86_64-pc-windows-msvc.exe'
+$url        = 'https://github.com/sarub0b0/kubetui/releases/download/v1.11.0/kubetui-x86_64-pc-windows-msvc.exe'
 
-$checksum = '29fdde52d152ade7e466bd439a5f1f12628ccf7a37b929024cc28b9ac1180c3c'
+$checksum = 'ce2e5ba43d0b818dc535c6cb11d789c0220fb220a1ffe546ddd0a1c144d02659'
 
 Get-ChocolateyWebFile $packageName "$toolsDir\kubetui.exe" $url $url64 -Checksum $checksum -ChecksumType 'SHA256'
